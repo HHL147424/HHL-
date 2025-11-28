a8ee8bc3bc7931445b5b6b0e9158c4e338e039b6
HHL147424 <3241055271@qq.com>
2025-11-28 18:46:55 +0800

    change upload

--- a.c
+++ a.c
@@ -2,6 +2,14 @@ include<stdio.h>
 
 int main()
 {
-	printf("test.......");
+	printf("1test.......");
+	printf("2test.......");
+	printf("3test.......");
+	printf("4test.......");
+	printf("5test.......");
+	printf("6test.......");
+	printf("7test.......");
+	printf("8test.......");
+	printf("9test.......");
 	return 0;
 }
